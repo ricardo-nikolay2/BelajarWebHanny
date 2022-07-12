23728c37b97b4ec4b79ae88dac6146acd607941a
Ricardo Nikolay (EDP Operator) <Ricardo@istw.co.id>
2022-07-12 15:14:17 +0700

    menambahkan kelas untuk rumus dan form login

---
+++ BelajarWeb/BelajarWeb/Rumus/Rumus.cs
@@ -0,0 +1,32 @@
+﻿namespace BelajarWeb.Rumus
+{
+    public class Rumus
+    {
+        public decimal HitungberatPipa(decimal Od,decimal thickness, decimal length,decimal quantity)
+        {
+            decimal hasil;
+
+            hasil = ((Od - thickness) * thickness * 0.02466m * length * quantity / 1000);
+            hasil = Math.Round(hasil, 2);
+
+            return hasil;
+        }
+
+
+        public string HitunghargaSetelahDiskon(decimal harga, decimal diskon, decimal hargaDiskon, decimal hasil)
+        {
+            string hargasetelahdiskon;
+
+            hargaDiskon = (harga * diskon / 100);
+
+            hasil = harga - hargaDiskon;
+            hargasetelahdiskon = hasil.ToString("0,0");
+
+            return hargasetelahdiskon;
+        }
+
+
+
+
+    }
+}
